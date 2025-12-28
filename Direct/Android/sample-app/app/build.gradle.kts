9b094dfd88d38078f5b70856bde47e52b79f6567
kfir-raph <kfir-raph@users.noreply.github.com>
2025-12-28 08:57:01 +0000

    Update Direct/Android README & changelog for 10.1.6

--- Direct/Android/sample-app/app/build.gradle.kts
+++ Direct/Android/sample-app/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 21
         targetSdk = 35
         versionCode = 1
-        versionName = "10.1.0"
+        versionName = "10.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-    implementation("co.prado.sdk:prado-android-native:10.0.2")
+    implementation("co.prado.sdk:prado-android-native:10.1.6")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
